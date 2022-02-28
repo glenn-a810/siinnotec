f2ba2b6f26137891129c78cf3ecd7b16e37a98a8
Glenn <a810.glenn@gmail.com>
2022-02-28 19:34:31 +0900

    컨텐츠 및 위치 설정중

--- src/App.vue
+++ src/App.vue
@@ -48,10 +48,63 @@
     </div>
   </div>
 
-  <div>
-    <span>Service Outline</span>
-    <span> | </span>
-    <span>Recent Projects</span>
+  <div class="parent">
+    <div class="child">
+      <p>Service Outline</p>
+      <hr />
+      <p>Business consulting</p>
+      <p>
+        We provide consulting of Development of organization and human
+        resources, Evaluation of business management, Development of new product
+        and commercialization support, Development of defense-related product
+        and commercialization support, Establishment support consulting.
+      </p>
+      <hr />
+      <p>Product consulting of Shipyard</p>
+      <p>
+        We provide consulting of improvement of the shipyard productivity,
+        equipment support and planning / design of production plants and ship
+        building.
+      </p>
+      <hr />
+      <p>ICT Business</p>
+      <p>
+        We provide ICT consulting of construction of major facilities of
+        security systems, mobile communications, smart-city, fusion of
+        information and communication, such as the construction of production,
+        materials, and personnel management system.
+      </p>
+      <hr />
+      <p>Supply of shipbuilding, marine, and new technology equipments</p>
+      <ul>
+        <li>Shipyard equipments and materials</li>
+        <li>Shipbuilding equipments and materials</li>
+        <li>
+          Radar with ECDIS(Electronic Chart Display and Information System
+        </li>
+        <li>Ship ECS(Engine Control System)</li>
+        <li>The materials of new technology and renewable energy</li>
+      </ul>
+      <hr />
+    </div>
+    <div class="child">
+      <p>Recent Projects</p>
+      <hr />
+      <div class="parent">
+        <div class="child">
+          <img src="./assets/6.jpg" alt="main_project1" />
+          <br />
+          <img src="./assets/10.jpg" alt="main_project2" />
+        </div>
+        <div class="child">
+          <p>
+            Performed consulting for PT PAL Indonesian National Shipyard’s
+            regarding production system and equipment improvement.
+          </p>
+          <p>Performed consulting established DSME’s ERP interface system</p>
+        </div>
+      </div>
+    </div>
   </div>
 
   <div>
@@ -98,7 +151,7 @@ export default {
 
 .overview {
   width: 50%;
-  height: 100%;
+  height: 90%;
 }
 
 .parent {
